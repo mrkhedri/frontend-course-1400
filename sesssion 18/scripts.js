4dc52d9168eed96df910c978d96118684e77871a
Mohammadreza Khedri <mohammadreza2800@gmail.com>
2022-02-05 21:44:25 +0330

    feat: session 18 added

---
+++ sesssion 18/scripts.js
@@ -0,0 +1,13 @@
+function changeFontSize() {
+  document.getElementById('font-size').style.fontSize='35px'
+}
+
+console.log(5 * 6)
+
+function showMeDateAndTimeWithAlert() {
+  window.alert(Date())
+}
+
+function showMeDateAndTimeWithDocument() {
+  document.write('<div style="color:#ff4444">' + Date() + '</div>')
+}
